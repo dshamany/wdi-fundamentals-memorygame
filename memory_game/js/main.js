a9aa9e1780b3fe8e64ba37a5574cce55f916291a
Daniel Shamany <dshamany@gmail.com>
2019-09-25 14:42:17 -0700

    Created some game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,13 @@
+console.log("Up and Running");
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] == cardsInPlay[1])
+{
+    alert("You Found a Match!");
+}
+else
+{
+    alert("Sorry, Try Again.");
+}
