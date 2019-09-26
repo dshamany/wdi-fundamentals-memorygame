d6d323abc5e818f3e5d7b9eb632677e01f66199b
Daniel Shamany <dshamany@gmail.com>
2019-09-26 11:07:11 -0700

    created objects to hold cards and cleaned up functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,32 @@
 console.log("Up and Running");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "Queen",
+        suit: "Hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "Queen",
+        suit: "Diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "King",
+        suit: "Hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "King",
+        suit: "Diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+]
 var cardsInPlay = [];
 
 function checkForMatch()
 {
-    if (cardsInPlay[0] === cardsInPlay[1])
+    if (cardsInPlay[0].rank === cardsInPlay[1].rank)
     {
         alert("You Found a Match!");
     }
@@ -17,15 +38,15 @@ function checkForMatch()
 
 function flipCard(cardId)
 {
-    console.log("User Flipped " + cards[cardId]);
+    console.log("User Flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
 
     cardsInPlay.push(cards[cardId]);
 
     if (cardsInPlay.length === 2)
     {
        checkForMatch();
+       cardsInPlay = [];
     }
 }
 
-flipCard(0);
-flipCard(1);
+// flipCard(0);
